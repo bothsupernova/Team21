974c0729a880f135ab8b57c32c5401600f84171b
Naphat Somboonhansa <naphat@blowfish.cs.colostate.edu>
2024-05-04 18:15:30 -0600

    1

--- team_21/about.php
+++ team_21/about.php
@@ -42,6 +42,23 @@
                 <p> </p>
             </div>
         </div>
+        </div>
+
+        <div class="NaphatS">
+        <h3>
+            Naphat Somboonhansa
+        </h3>
+        <img src="./assets/images/members/naphat.jpg" alt="Picture of Naphat">
+        <p>
+        I am a software engineer with an ESTP personality type, and I love playing basketball, tennis, and football. I believe I can change the world, and I will make it. I am the Chief Technology Officer of a startup called Carterapp Co., Ltd. Our company partners with Shark Tank, Sasin at Chulalongkorn University, 
+        Interzone Thammasat University, TED fund, and others. 
+        </p>
+        <p>
+        In the past, I have worked part-time at a tech company called Bluesharp, as well as at Sairee Hut Resort as a server. Additionally, 
+        I worked as a chef at Cafe de Bangkok in Fort Collins, USA. I am a member of the 8th generation of the Thailand-Japan cultural ambassador program, and I also joined the competitive team for the World Robot Olympiad in 2018.
+        </p>
+
+        </div>
     </div>
 </body>
 
